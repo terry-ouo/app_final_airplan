1a20dcd14a1b52bc78e102b2d27956e26a97c389
Stinckygoo <91928322+Stinckygoo@users.noreply.github.com>
2022-05-25 12:12:15 +0800

    V2

--- app/src/main/java/com/example/app_airplan/MainActivity.kt
+++ app/src/main/java/com/example/app_airplan/MainActivity.kt
@@ -7,27 +7,74 @@ import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.SurfaceHolder
 import com.example.app_airplan.databinding.ActivityMainBinding
 import android.view.SurfaceView
+import android.view.View
+import kotlinx.coroutines.*
+
 class MainActivity : AppCompatActivity() {
+    lateinit var job: Job
     lateinit var binding: ActivityMainBinding
+    var game_status = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.start.setOnClickListener {
+            game_status = !game_status
+            job = GlobalScope.launch(Dispatchers.Main) {
+                while (game_status) {
+                    delay(25)
+                    val canvas: Canvas = binding.surfaceView.holder.lockCanvas()
+                    binding.surfaceView.drawSomething(canvas)
+                    binding.surfaceView.holder.unlockCanvasAndPost(canvas)
+                    if (!game_status) {
 
+                    }
+                    if (game_status) {
+
+                    }
+                }
+
+
+            }
+        }
     }
+    override fun onPause() {
+        super.onPause()
+        job.cancel()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (!game_status){
+            job = GlobalScope.launch(Dispatchers.Main) {
+                delay(25)
+                val canvas: Canvas = binding.surfaceView.holder.lockCanvas()
+                binding.surfaceView.drawSomething(canvas)
+                binding.surfaceView.holder.unlockCanvasAndPost(canvas)
+            }
+            }
+        }
+
+
+
 }
-class MySurfaceview(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),SurfaceHolder.Callback2 {
+
+class MySurfaceView(context: Context?, attrs: AttributeSet?) : SurfaceView(context, attrs),
+    SurfaceHolder.Callback2 {
+    var BGmoveX: Int = 0
     var surfaceHolder: SurfaceHolder = holder
     var BG: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.background)
 
     init {
         surfaceHolder.addCallback(this)
     }
+
     override fun surfaceCreated(p0: SurfaceHolder) {
         var canvas: Canvas = surfaceHolder.lockCanvas()
         drawSomething(canvas)
@@ -36,8 +83,22 @@ class MySurfaceview(context: Context?, attrs: AttributeSet?) : SurfaceView(conte
 
     }
 
-    private fun drawSomething(canvas: Canvas) {
-        canvas.drawBitmap(BG, 0f, 0f, null)
+    fun drawSomething(canvas: Canvas) {
+        //canvas.drawBitmap(BG, 0f, 0f, null)
+        BGmoveX--
+        var BGnewX: Int = BG.width + BGmoveX
+
+        // 如果已捲動整張圖，則重新開始
+        if (BGnewX <= 0) {
+            BGmoveX = 0
+            // only need one draw
+            canvas.drawBitmap(BG, BGmoveX.toFloat(), 0f, null)
+        } else {
+            // need to draw original and wrap
+            canvas.drawBitmap(BG, BGmoveX.toFloat(), 0f, null)
+            canvas.drawBitmap(BG, BGnewX.toFloat(), 0f, null)
+        }
+
     }
 
     override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
